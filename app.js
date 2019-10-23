efcd33b437ccab0920b748ff4266148c2fdfbaa6
Nguyễn Văn Nhật <1612457@student.hcmus.edu.vn>
2019-10-23 10:35:14 +0700

    Chỉnh sửa để Project Caro có thể kết nối

--- app.js
+++ app.js
@@ -7,14 +7,23 @@ var bodyParser = require('body-parser')
 var passport = require('passport');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/user');
-
+// var cors = require('cors');
 var app = express();
 
 require('./middleWares/passport')(passport);
 
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+}
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+app.use(allowCrossDomain)
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -34,6 +43,9 @@ app.use('/user', usersRouter);
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
+// Then use it before your routes are set up:
+// app.use(cors());
 
 // error handler
 app.use(function(err, req, res, next) {
